077f15113e922138e765f685612335339b2caee7
Rob <robert.a.nickel@gmail.com>
2022-06-24 16:59:45 +1200

    Enum script and folders
    Moved enums into specific file, created additional folders for content

---
+++ Assets/Scripts/Accessory/Enumerators.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Role { WoodCutter, Miner, Gatherer, Builder, Labourer }
+public enum Need { Food, Clothing, Tool, Entertainment, Rest }
+
+public enum ItemCategory { Food, Clothing, Tool, Beverage }
